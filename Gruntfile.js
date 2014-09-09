41235e7e35d0aef555675c25c7c4ca7871903485
snrbrnjna <snr@brnjna.net>
2014-09-09 23:51:40 +0200

    chore(grunt): added task for bumping a new release

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,33 @@
 
 module.exports = function( grunt ) {
 
-  var banner = ( function() {
-    var src = grunt.file.read('fluid-masonry.js');
+  // src: content of unpackaged script
+  function getBanner(src) {
     var re = new RegExp('^\\s*(?:\\/\\*[\\s\\S]*?\\*\\/)\\s*');
     var matches = src.match( re );
     var banner = matches[0].replace( 'Fluid Masonry', 'Fluid Masonry PACKAGED' );
     return banner;
-  })();
+  }
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: getBanner(grunt.file.read('fluid-masonry.js')),
 
     jshint: {
       src: [ 'fluid-masonry.js' ],
       options: grunt.file.readJSON('.jshintrc')
+    },
+
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        push: true,
+        pushTo: 'origin',
+        commit: true,
+        commitFiles: ['-a']
+      }
     },
 
     requirejs: {
@@ -34,7 +48,7 @@ module.exports = function( grunt ) {
             jquery: 'empty:'
           },
           wrap: {
-            start: banner
+            start: '<%= banner %>'
           }
         }
       }
@@ -46,7 +60,7 @@ module.exports = function( grunt ) {
           'dist/fluid-masonry.pkgd.min.js': [ 'dist/fluid-masonry.pkgd.js' ]
         },
         options: {
-          banner: banner
+          banner: '<%= banner %>'
         }
       }
     }
@@ -56,6 +70,24 @@ module.exports = function( grunt ) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.registerTask('updateVersion', function() {
+    var file = 'fluid-masonry.js';
+    var content = grunt.file.read(file);
+    var version = grunt.config.get('pkg.version');
+    var re = new RegExp(/Fluid Masonry v(\d+\.\d+\.\d+)/);
+    var match = content.match(re);
+    if (match && match[1] !== version) {
+      content = content.replace(re, 'Fluid Masonry v' + version);
+      grunt.file.write(file, content);
+      // update banner
+      grunt.config.set('banner', getBanner(content));
+      grunt.log.writeln('version update from ' + match[1] + ' to ' + version);
+    } else {
+      grunt.log.writeln('version unchanged: ' + version);
+    }
+  });
 
   grunt.registerTask( 'pkgd-edit', function() {
     var outFile = grunt.config.get('requirejs.pkgd.options.out');
@@ -67,9 +99,26 @@ module.exports = function( grunt ) {
 
   grunt.registerTask( 'default', [
     'jshint',
+    'updateVersion',
     'requirejs',
     'pkgd-edit',
     'uglify'
   ]);
+
+  grunt.registerTask( 'release', function(target) {
+    if (target) {
+      grunt.task.run([
+        'jshint',
+        'bump-only:' + target,
+        'updateVersion',
+        'requirejs',
+        'pkgd-edit',
+        'uglify'
+      ]);
+      grunt.log.writeln('check the test cases and commit (-a) and push (--tags) the new version, if everything is fine.');
+    } else {
+      grunt.warn('no target given for bump task. Has to be one of "patch", "minor", "mayor"');
+    }
+  });
 
 };
